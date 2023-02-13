e9fd7e1db6bbfd4b611de6b7f0d123cc64d0442f
Jaris <john.aristosa@hotmail.com>
2023-02-13 11:39:13 +0100

    feat(Clean + font): Clean structure + fonts

---
+++ lib/screens/spashscreen_wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:redditech/screens/home.dart';
+
+class SpashScreenWrapper extends StatelessWidget {
+  const SpashScreenWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeScreen();
+  }
+}
