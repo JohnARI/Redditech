247dc54425bc9296139a37722b209acdfa327040
Jaris <john.aristosa@hotmail.com>
2023-02-14 11:27:07 +0100

    Fix(Implement post preview): implement post preview on the application

--- lib/common/const.dart
+++ lib/common/const.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:redditech/components/home_page.dart';
+
+import '../components/postPreview.dart';
 
 // Constants of colors
 
@@ -26,7 +26,17 @@ const neutralLight0 = Color(0xffF8F9FA);
 
 
 const screens = [
-  Text('Home'),
+  PostPreview(
+                subreddit: 'r/FlutterDev',
+                username: 'u/FlutterDev',
+                title: 'Fuck flutter',
+                profilePicture: 'https://googleflutter.com/sample_image.jpg',
+                image: 'https://googleflutter.com/sample_image.jpg',
+                timestamp: 1620000000,
+                upVotes: 100,
+                downVotes: 0,
+                comments: 10,
+              ),
   Text('Communities'),
   Text('Add'),
   Text('Search'),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:redditech/components/bottom_navbar.dart';
 import 'package:redditech/components/appbar.dart';
 import 'package:redditech/common/const.dart';
-import 'package:redditech/components/postPreview.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -27,21 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
           title: 'home',
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              // screens[_selectedIndex]
-
-              PostPreview(
-                subreddit: 'r/FlutterDev',
-                username: 'u/FlutterDev',
-                title: 'Fuck flutter',
-                profilePicture: 'https://googleflutter.com/sample_image.jpg',
-                image: 'https://googleflutter.com/sample_image.jpg',
-                timestamp: 1620000000,
-                upVotes: 100,
-                downVotes: 0,
-                comments: 10,
-              ),
-            ],
+            children: [screens[_selectedIndex]],
           )),
       bottomNavigationBar: BottomNavbar(
         onSelectedIndexChanged: _onSelectedIndexChanged,
