139170eb463a06150c4c7cb620e994618cfc1891
LeguyDany <85669825+LeguyDany@users.noreply.github.com>
2023-02-27 14:27:22 +0100

    feat(subreddit): Merge

---
+++ lib/services/api_subreddits.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:draw/draw.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'package:redditech/screens/main_screen.dart';
+import 'package:redditech/services/api.dart';
+import 'package:redditech/services/api_client.dart';
+import 'package:redditech/services/authentication.dart';
+
+class ApiSubreddit {
+  // Future<bool> test() async {
+  //   try {
+  //     // print(authUrl);
+  //     // String? value = await storage.read(key: "token");
+  //     // await reddit?.auth.authorize(value.toString());
+
+  //     Redditor? me = await reddit?.user.me();
+  //     print(me);
+  //     return true;
+  //   } catch (exception) {
+  //     print("bonjour");
+  //     print(exception);
+  //     return false;
+  //   }
+  // }
+}
+
+ApiSubreddit subreddit = ApiSubreddit();
