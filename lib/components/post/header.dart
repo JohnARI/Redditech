33ed4019315eda2176e734da98fa7716b25424bf
Lxdovic <ludovicdebever0@gmail.com>
2023-02-27 12:27:11 +0100

    feat(Post): Added post page

---
+++ lib/components/post/header.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:redditech/common/const.dart';
+import 'package:redditech/components/post/header_top.dart';
+import 'package:redditech/components/post/header_bottom.dart';
+
+class Header extends StatefulWidget {
+  const Header(
+      {super.key,
+      required this.profileSrc,
+      required this.username,
+      required this.title});
+
+  final String profileSrc;
+  final String username;
+  final String title;
+
+  @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      HeaderTop(profileSrc: widget.profileSrc, username: widget.username),
+      HeaderBottom(title: widget.title),
+    ]);
+  }
+}
